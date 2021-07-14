fdc369edc48c0b4f029e91450c3b7e527e781c84
Sayan Biswas <sayan@pokurt.me>
2021-07-15 01:13:54 +0530

    lewd: take a modular approach this time around.
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>

---
+++ chizuru/bot/lewd.go
@@ -0,0 +1,39 @@
+package bot
+
+import (
+	"Chizuru-GO/chizuru/nekosAPI"
+	"fmt"
+	"github.com/PaulSonOfLars/gotgbot/v2"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext"
+)
+
+func nekosLIFE(b *gotgbot.Bot, ctx *ext.Context) (err error) {
+	args := ctx.Args()
+	if len(args) == 1 {
+		_, err = ctx.EffectiveMessage.Reply(b, "No argument provided.", &gotgbot.SendMessageOpts{ParseMode: "html"})
+		return err
+	}
+
+	sat := false
+	for _, en := range nekosAPI.EndPoints {
+		if en == args[1] {
+			sat = true
+			pic, err := nekosAPI.DoRequest(args[1])
+			if err != nil {
+				_, err = ctx.EffectiveMessage.Reply(b, fmt.Sprintf("Error: <code>%s</code>", err.Error()), &gotgbot.SendMessageOpts{ParseMode: "html"})
+				return err
+			}
+			_, err = b.SendAnimation(ctx.EffectiveChat.Id, pic.URL, &gotgbot.SendAnimationOpts{})
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	if sat == false {
+		_, err = ctx.EffectiveMessage.Reply(b, fmt.Sprintf("<code>%s</code> is not a valid endpoint\nSupported endpoints:\n <code>%s</code>", args[1], nekosAPI.EndPoints), &gotgbot.SendMessageOpts{ParseMode: "html"})
+		return err
+	}
+
+	return nil
+}
