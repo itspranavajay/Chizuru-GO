6e128885cc63e7b852b0c0ee6e17ae9d2429d407
Sayan Biswas <sayan@pokurt.me>
2021-07-15 01:12:56 +0530

    request: add endpoints in the form of a string dict
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>

--- chizuru/nekosAPI/request.go
+++ chizuru/nekosAPI/request.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 )
 
-func DoRequest(target string)(res *Response, err error){
+func DoRequest(target string) (res *Response, err error) {
 	reqURL := fmt.Sprintf("%s/%s", baseUrl, target)
 	resp, err := http.Get(reqURL)
 	if err != nil {
@@ -28,3 +28,5 @@ func DoRequest(target string)(res *Response, err error){
 	}
 	return data, nil
 }
+
+var EndPoints = []string{"solog", "smug", "feet", "smallboobs", "lewdkemo", "woof", "gasm", "solo", "8ball", "goose", "cuddle", "avatar", "cum", "slap", "les", "v3", "erokemo", "bj", "pwankg", "nekoapi_v3.1", "ero", "hololewd", "pat", "gecg", "holo", "poke", "feed", "fox_girl", "tits", "nsfw_neko_gif", "eroyuri", "holoero", "pussy", "Random_hentai_gif", "lizard", "yuri", "keta", "neko", "hentai", "feetg", "eron", "erok", "baka", "kemonomimi", "hug", "cum_jpg", "nsfw_avatar", "erofeet", "meow", "kiss", "wallpaper", "tickle", "blowjob", "spank", "kuni", "classic", "waifu", "femdom", "boobs", "trap", "lewd", "pussy_jpg", "anal", "futanari", "ngif", "lewdk"}
