8110dd5818d367a5a242f1a4b030f63ebefdc880
Sayan Biswas <sayan@pokurt.me>
2021-07-15 01:13:29 +0530

    start: fixup
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>

--- chizuru/bot/start.go
+++ chizuru/bot/start.go
@@ -5,17 +5,16 @@ import (
 	"fmt"
 	"github.com/PaulSonOfLars/gotgbot/v2"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
-
 )
 
-func startCMD(b *gotgbot.Bot, ctx *ext.Context)(err error){
+func startCMD(b *gotgbot.Bot, ctx *ext.Context) (err error) {
 	if ctx.Message.Chat.Type == "private" {
-		_, err = ctx.EffectiveMessage.Reply(b, fmt.Sprintf("Hello, I'm %s, I fetch lewds just for you >/////<", b.User.FirstName), &gotgbot.SendMessageOpts{
+		_, err = ctx.EffectiveMessage.Reply(b, fmt.Sprintf("Hello, I'm %s, I fetch lewds just for you UwU\nTap on /help to know more!", b.User.FirstName), &gotgbot.SendMessageOpts{
 			ParseMode: "html",
 			ReplyMarkup: gotgbot.InlineKeyboardMarkup{
 				InlineKeyboard: [][]gotgbot.InlineKeyboardButton{{
 					{Text: "Channel", Url: config.ChannelLINK},
-					{Text: "Chat", Url: config.ChannelLINK},
+					{Text: "Chat", Url: config.ChatLINK},
 				}},
 			},
 		})
