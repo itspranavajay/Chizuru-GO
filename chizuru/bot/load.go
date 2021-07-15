cde34476d6203b54ad144e60f7f45b6ce12bdda1
Sayan Biswas <sayan@pokurt.me>
2021-07-15 23:54:18 +0530

    Add handlers.

--- chizuru/bot/load.go
+++ chizuru/bot/load.go
@@ -3,6 +3,7 @@ package bot
 import (
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers/filters"
 )
 
 func LoadHandlers(d *ext.Dispatcher) {
@@ -10,4 +11,6 @@ func LoadHandlers(d *ext.Dispatcher) {
 	d.AddHandler(startCmd)
 	d.AddHandler(handlers.NewCommand("nekos", nekosLIFE))
 	d.AddHandler(handlers.NewCommand("help", getHelp))
+	d.AddHandler(handlers.NewCommand("stats", getStats))
+	d.AddHandler(handlers.NewMessage(filters.All, logStuff))
 }
