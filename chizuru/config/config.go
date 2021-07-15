8c6c8991876569e65bbc29d61bdb8d45b7dabca8
Sayan Biswas <sayan@pokurt.me>
2021-07-15 23:49:07 +0530

    Add OwnerID and Dburl to config
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>

--- chizuru/config/config.go
+++ chizuru/config/config.go
@@ -5,13 +5,23 @@ import (
 	"log"
 )
 
-
 var ChatLINK string
 var ChannelLINK string
+var DBUrl string
+var OwnerID int64
 
-
-func SetVars(p *configparser.ConfigParser){
+func SetVars(p *configparser.ConfigParser) {
 	var err error
+	DBUrl, err = p.Get("postgres", "url")
+	if err != nil {
+		log.Fatal("an error in getting postgresql database connection url: ", err)
+	}
+
+	OwnerID, err = p.GetInt64("bot_admins", "OwnerID")
+	if err != nil {
+		log.Fatal("an error in getting owner id: ", err)
+	}
+
 	ChatLINK, err = p.Get("telegram_links", "chat")
 	if err != nil {
 		log.Fatal("an error in getting chat link: ", err)
@@ -20,4 +30,5 @@ func SetVars(p *configparser.ConfigParser){
 	if err != nil {
 		log.Fatal("an error in getting channel link: ", err)
 	}
+
 }
