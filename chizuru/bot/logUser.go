5f0a5ed52c36a88103a7fefe154b7e8002aa533e
Aliwoto <aminnimaj@gmail.com>
2021-07-15 20:49:03 +0000

    fixed types problems
    Signed-off-by: Aliwoto <aminnimaj@gmail.com>

--- chizuru/bot/logUser.go
+++ chizuru/bot/logUser.go
@@ -2,13 +2,17 @@ package bot
 
 import (
 	"Chizuru-GO/chizuru/database"
+	"strconv"
+
 	"github.com/PaulSonOfLars/gotgbot/v2"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
 )
 
-func logStuff(b *gotgbot.Bot, ctx *ext.Context)(err error){
+func logStuff(b *gotgbot.Bot, ctx *ext.Context) (err error) {
 	u := ctx.EffectiveMessage.From
 	c := ctx.EffectiveChat
-	database.UpdateUser(u.Id, u.Username, string(c.Id), c.Title)
+
+	database.UpdateUser(u.Id, u.Username,
+		strconv.FormatInt(c.Id, 10), c.Title)
 	return nil
 }
