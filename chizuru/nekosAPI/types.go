2ac59643b7dfffcd40bc1c036577c1d3fb0655fb
RedAura <86167689+MoeZilla@users.noreply.github.com>
2021-07-18 13:51:40 +0530

    Update types.go

--- chizuru/nekosAPI/types.go
+++ chizuru/nekosAPI/types.go
@@ -4,3 +4,9 @@ type Response struct {
 	URL string `json:"url"`
 	Msg string `json:"msg"`
 }
+
+type TEXT struct {
+	Why string                 `json:"why"`
+        Name string                 `json:"name"`
+        Msg string                 `json:"msf"`
+}
